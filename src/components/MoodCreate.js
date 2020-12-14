d2e75090d6a6da4acf388d6233de94010f59f737
ctavispost <ctavispost@gmail.com>
2020-12-13 23:39:16 -0800

    Simplify forms in MoodCreate

--- src/components/MoodCreate.js
+++ src/components/MoodCreate.js
@@ -4,11 +4,11 @@ import PostModel from '../models/post';
 class MoodCreate extends Component {
     state = {
         colorId: null
-    }
+    };
 
     handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({colorId: event.target.dafaultValue});
+        this.setState({colorId: event.target.dataset.value});
         const newPost = {
             colorId: this.state.colorId
         };
@@ -22,32 +22,27 @@ class MoodCreate extends Component {
                 <h4>How are you?</h4>
                 <section className="flexy">
                     <form onSubmit={this.handleSubmit}>
-                        <input type="number" defaultValue="1" style={{display: "none"}}></input>
-                        <button type="submit" value="Submit">
+                        <button type="submit" value="Submit" data-value="1">
                             <i className="material-icons orange">sentiment_very_satisfied</i>
                         </button>
                     </form>
                     <form onSubmit={this.handleSubmit}>
-                        <input type="number" defaultValue="2" style={{display: "none"}}></input>
-                        <button type="submit" value="Submit">
+                        <button type="submit" value="Submit" data-value="2">
                             <i className="material-icons green">sentiment_satisfied</i>
                         </button>
                     </form>
                     <form onSubmit={this.handleSubmit}>
-                        <input type="number" defaultValue="3" style={{display: "none"}}></input>
-                        <button type="submit" value="Submit">
+                        <button type="submit" value="Submit" data-value="3">
                             <i className="material-icons purple">sentiment_neutral</i>
                         </button>
                     </form>
                     <form onSubmit={this.handleSubmit}>
-                        <input type="number" defaultValue="4" style={{display: "none"}}></input>
-                        <button type="submit" value="Submit">
+                        <button type="submit" value="Submit" data-value="4">
                             <i className="material-icons blue">seniment_dissatisfied</i>
                         </button>
                     </form>
                     <form onSubmit={this.handleSubmit}>
-                        <input type="number" defaultValue="5" style={{display: "none"}}></input>
-                        <button type="submit" value="Submit">
+                        <button type="submit" value="Submit" data-value="5">
                             <i className="material-icons grey">sentiment_very_dissatisfied</i>
                         </button>
                     </form>
