8a453a24625a55ba3aaebab54f0a8c776370d48e
ctavispost <ctavispost@gmail.com>
2020-12-15 21:22:57 -0800

    Add method to fetch all colors

--- src/models/color.js
+++ src/models/color.js
@@ -1,6 +1,11 @@
 const url = `http://localhost:4000/api/v1`;
 
 class ColorModel {
+    static all = () => {
+        return fetch(`${url}/color/`)
+            .then(res => res.json());
+    };
+
     static getColor = (colorId) => {
         return fetch(`${url}/color/${ colorId }`)
             .then(res => res.json())
