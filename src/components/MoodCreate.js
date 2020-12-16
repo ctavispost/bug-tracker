f9f6c5ef06d885d63b286acfeb11adc42063354b
ctavispost <ctavispost@gmail.com>
2020-12-15 16:46:24 -0800

    Wrap MoodCreate buttons in div so model closes on button press

--- src/components/MoodCreate.js
+++ src/components/MoodCreate.js
@@ -4,15 +4,13 @@ import PostModel from '../models/post';
 class MoodCreate extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            see: this.props.show
-        };
+        
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick = async (event) => {
         event.preventDefault();
-        event.persist();
+        //event.persist();
         
         let colorIdStr = null;
         if(event.target.dataset.value) {
@@ -23,13 +21,15 @@ class MoodCreate extends Component {
         };
         
         const colorVal = parseInt(colorIdStr);
-        
+        const localUser = parseInt(localStorage.getItem('id'));
         const newPost = {
-            colorId: colorVal
+            colorId: colorVal,
+            userId: localUser
         };
+        
         console.log("new post", newPost);
-        this.props.onClick(await PostModel.create(newPost));
-    }  
+        await PostModel.create(newPost);
+    }
 
     render() {
         let showBlock = {display: "none"};
@@ -42,25 +42,47 @@ class MoodCreate extends Component {
             <section className="modal" id="modal1" style={ showBlock }>
                 <h1 className="just-center pad-top marg-bot-sm">How are you?</h1>
                 <section className="flexy pad-bot">
-                    <button onClick={this.handleClick} value="Submit" data-value="1" className="flexy just-center align-center round btn-quiet">
-                        <i className="material-icons round pad-med orange">sentiment_very_satisfied</i>
-                    </button>
+                    <div onClick={this.props.getOut}>
+                        <div>
+                            <button onClick={this.handleClick} value="Submit" data-value="1" className="flexy just-center align-center round btn-quiet">
+                                <i className="material-icons round pad-med orange">sentiment_very_satisfied</i>
+                            </button>
+                        </div>
+                    </div>
                     
-                    <button onClick={this.handleClick} value="Submit" data-value="2" className="flexy just-center align-center round btn-quiet">
-                        <i className="material-icons round pad-med green">sentiment_satisfied</i>
-                    </button>
+                    <div onClick={this.props.getOut}>
+                        <div>
+                            <button onClick={this.handleClick} value="Submit" data-value="2" className="flexy just-center align-center round btn-quiet">
+                                <i className="material-icons round pad-med green">sentiment_satisfied</i>
+                            </button>
+                        </div>
+                    </div>
                     
-                    <button onClick={this.handleClick} value="Submit" data-value="3" className="flexy just-center align-center round btn-quiet">
-                        <i className="material-icons round pad-med purple">sentiment_neutral</i>
-                    </button>
-                    
-                    <button onClick={this.handleClick} value="Submit" data-value="4" className="flexy just-center align-center round btn-quiet">
-                        <i className="material-icons round pad-med blue">sentiment_dissatisfied</i>
-                    </button>
-                    
-                    <button onClick={this.handleClick} value="Submit" data-value="5" className="flexy just-center align-center round btn-quiet">
-                        <i className="material-icons round pad-med grey">sentiment_very_dissatisfied</i>
-                    </button>
+                    <div onClick={this.props.getOut}>
+                        <div>
+                            <button onClick={this.handleClick} value="Submit" data-value="3" className="flexy just-center align-center round btn-quiet">
+                                <i className="material-icons round pad-med purple">sentiment_neutral</i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div onClick={this.props.getOut}>
+                        <div>
+                        <button onClick={this.handleClick} value="Submit" data-value="4" className="flexy just-center align-center round btn-quiet">
+                            <i className="material-icons round pad-med blue">sentiment_dissatisfied</i>
+                        </button>
+                        </div>
+                    </div>
+
+                    <div onClick={this.props.getOut}>
+                        <div>
+                            <button onClick={this.handleClick} value="Submit" data-value="5" className="flexy just-center align-center round btn-quiet">
+                                <i className="material-icons round pad-med grey">sentiment_very_dissatisfied</i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <button onClick={this.props.getOut}>x</button>
                 </section>
             </section>
         );    
