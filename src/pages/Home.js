bfe74eac07a2e1b92a2b558e81b77e17900c02c7
ctavispost <ctavispost@gmail.com>
2020-12-15 16:44:06 -0800

    Set fetchData to run on componentDidUpdate, so that new posts appear on screen after entry

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,17 +10,22 @@ class Home extends Component {
     posts: [],
     userPosts: [],
     show1: false,
-    show2: false
+    show2: false,
+    postId: null
   }
 
   componentDidMount() {
     this.fetchData();
   };
 
+  componentDidUpdate() {
+    this.fetchData();
+  }
+
   //get all posts, all of current user's posts, and set colors for them, then set state
   fetchData = async () => {
-    const allPosts = (await PostModel.all()).posts;
-    const allUserPosts = (await PostModel.all()).posts;
+    const allPosts = (await PostModel.all()).posts.reverse();
+    const allUserPosts = (await PostModel.all()).posts.reverse();
     for (let p of allPosts) {
       p.colorHex = (await ColorModel.getColor(p.colorId)).color.hex;
     }
@@ -34,13 +39,25 @@ class Home extends Component {
     this.setState({ show1: true });
   };
 
-  handleEdit = () => {
-    const elems = document.getElementById('#modal2');
+  handleEdit = (e) => {
+    this.setState({ show2: true });
   };
 
-  newPost = (post) => {
-    this.setState({ posts: [...this.state.posts, post], userPosts: [...this.state.userPosts, post], show1: false });
+  getOut = (event) => {
+    this.setState({ show1: false });
   };
+  
+  newPost = (event) => {
+    event.preventDefault();
+    //this.fetchData();
+  };
+
+  deletePost = (index) => {
+    this.setState({
+      posts: this.state.posts.filter((v, i)=> i !== index),
+      userPosts: this.state.userPosts.filter((v, i)=> i !== index)
+    })
+  }
 
   render(){
     const allPostList = this.state.posts.map((post, index) => {
@@ -49,11 +66,11 @@ class Home extends Component {
       );
     });
     
-    const userPostList = () =>{
+    const userPostList = () => {
       if (this.state.userPosts.length > 0) {
         this.state.userPosts.map((post, index) => {
           return (
-            <a className="modal-trigger" href="modal2" onClick={() => this.handleEdit()}>
+            <a className="modal-trigger" href="#modal2" onClick={e => this.handleEdit(e)}>
               <PostComp {...post} key={ post.id }/>
             </a>
           )
@@ -63,7 +80,6 @@ class Home extends Component {
       }
     }
       
-
     //in case of empty state or while loading
     const noUserPostList = (<p>To save your mood, press the button below.</p>);
 
@@ -71,26 +87,23 @@ class Home extends Component {
       <article className="gridy">
         <section className="gridy">
           <h1 className="just-center">Your moods</h1>
-          <section className="flexy">
+          <section className="flexy flex-reverse">
             { this.state.userPosts ? userPostList() : noUserPostList }
           </section>
         </section>
         <section className="gridy">
           <h1 className="just-center">Everyone's moods</h1>
-          <section className="flexy">
+          <section className="flexy flex-reverse">
             { this.state.posts ? allPostList : 'Loading...'}
           </section>
         </section>
 
         <a className="btn-floating btn waves-effect waves-light red modal-trigger add-btn" href="#modal1" onClick={e => this.handleCreate(e)}><i className="material-icons">add</i></a>
 
-        <MoodCreate onClick={post => this.newPost(post)} show={this.state.show1}/>
         
-        <div id="modal2" className="modal">
-          <div className="modal-content">
-            <MoodEdit />
-          </div>
-        </div>
+        <MoodCreate newPost={this.newPost} show={this.state.show1} getOut={this.getOut}/>
+        <MoodEdit show={this.state.show2} postId={this.props.postId}/>
+          
       </article>
     )
   }
