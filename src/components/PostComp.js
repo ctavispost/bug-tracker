2a343259b7be71ccab57aea10b06c4328a8a9812
ctavispost <ctavispost@gmail.com>
2020-12-15 22:05:41 -0800

    Make user posts into buttons which open MoodEdit modal

--- src/components/PostComp.js
+++ src/components/PostComp.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const PostComp = (props) => {
     const style = { backgroundColor: props.colorHex };
-    return (<div className="moodSquare" style={ style }></div>);
+
+    const btn = (<button className="moodSquare btn-quiet-two" onClick={props.onClick} style={ style }></button>);
+    const nonBtn = (<div className="moodSquare" style={ style }></div>);
+    
+    return ( props.onClick ? btn : nonBtn );
 }
 
 export default PostComp;
